9a55b265f0e23ea8a55a134932074083faf6b8f9
hcWang942 <hcwang942@gmail.com>
2025-11-26 00:02:40 +0100

    test ci

--- Projects/hcWang_project_test/Test.lean
+++ Projects/hcWang_project_test/Test.lean
@@ -1,7 +1,16 @@
 import Mathlib.Tactic
+import Mathlib.Combinatorics.SimpleGraph.Basic
+import Mathlib.Combinatorics.SimpleGraph.Finite
 
 theorem test_pass (a b : Nat) : a + b = b + a := by
   omega
 
 theorem test_sorry (a b : Nat) : a * b = b * a := by
   sorry
+
+theorem handshaking_lemma {V : Type*} [Fintype V] [DecidableEq V]
+  (G : SimpleGraph V) [DecidableRel G.Adj] :
+    Even (Finset.univ.sum (fun v => G.degree v)) := by
+  sorry
+
+#lint
