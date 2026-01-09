31ef685cba2e9c0124969e75aa5b0980bc276a1f
awueth <83429722+awueth@users.noreply.github.com>
2026-01-09 13:15:40 +0100

    another sorry falls

--- Projects/Wuethrich/List/Zip.lean
+++ Projects/Wuethrich/List/Zip.lean
@@ -2,8 +2,16 @@ import Mathlib.Algebra.Order.Group.Nat
 import Mathlib.Data.Nat.Lattice
 
 variable {α β γ δ : Type*}
+variable {a : α} {b : β} {c : γ}
+variable {as : List α} {bs : List β} {cs : List γ}
+variable {f : α → β → γ → δ}
 
 namespace List
+
+@[simp] theorem zipWith3_nil_left  : zipWith3 f [] bs cs = [] := rfl
+@[simp] theorem zipWith3_nil_middle : zipWith3 f as [] cs = [] := by simp [zipWith3]
+@[simp] theorem zipWith3_nil_right: zipWith3 f as bs [] = [] := by simp [zipWith3]
+@[simp] theorem zipWith3_cons_cons : zipWith3 f (a :: as) (b :: bs) (c :: cs) = f a b c :: zipWith3 f as bs cs := rfl
 
 @[simp, grind =] theorem length_zipWith3 {f : α → β → γ → δ} {l₁ : List α} {l₂ : List β} {l₃  : List γ} :
     length (zipWith3 f l₁ l₂ l₃) = min (min (length l₁) (length l₂)) (length l₃) := by
@@ -24,9 +32,16 @@ theorem getElem?_zipWith3_eq_some {f : α → β → γ → δ}
     {l₁ : List α} {l₂ : List β} {l₃ : List γ} {w : δ} {i : Nat} :
     (zipWith3 f l₁ l₂ l₃)[i]? = some w ↔
       ∃ x y z, l₁[i]? = some x ∧ l₂[i]? = some y ∧ l₃[i]? = some z ∧ f x y z = w := by
-  induction l₁ generalizing l₂ l₃ i
-  · simp [zipWith3]
-  · sorry --cases l₂ <;> cases l₃ <;> cases i <;> simp_all [zipWith3]
+  induction l₁ generalizing l₂ l₃ i with
+  | nil => simp [zipWith3]
+  | cons head₁ tail₁ ih₁ =>
+    cases l₂ with
+    | nil => simp
+    | cons head₂ tail₂ =>
+      cases l₃ with
+      | nil => simp
+      | cons head₃ tail₃ =>
+        cases i <;> simp_all
 
 @[simp, grind =]
 theorem getElem_zipWith3 {f : α → β → γ → δ} {l₁ : List α} {l₂ : List β} {l₃  : List γ}
